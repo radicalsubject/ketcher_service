d7e12b28cbb6bab8c8fc96286dc62c8c86a1fc26
radicalsubject <olegteravolt@gmail.com>
2021-03-25 16:22:40 +0300

    fixed dependencies on notebook

--- ketcher_container/packages/ketcher-react/src/contexts/index.ts
+++
@@ -1,5 +0,0 @@
-import SettingsContext, { ISettingsContext } from './settingsContext'
-import FormContext from './formContext'
-
-export { SettingsContext, FormContext }
-export type { ISettingsContext }
--- ketcher_container/packages/ketcher-react/src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './useSettingsContext'
-export * from './useResizeObserver'
-export * from './useFormContext'
