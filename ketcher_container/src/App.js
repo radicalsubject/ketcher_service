cfe93338f705b521f46aca9eda6fc870f34d476b
radicalsubject <olegteravolt@gmail.com>
2021-03-26 16:52:14 +0300

    i dont know what s happening

--- ketcher_container/src/App.js
+++ ketcher_container/src/App.js
@@ -9,39 +9,24 @@ import { Editor } from 'ketcher-react'
 // @ts-ignore
 import { RemoteStructServiceProvider } from 'ketcher-core'
 import 'ketcher-react/dist/index.css'
-// import styled from "styled-components"
 
-// (global as any).Miew = Miew
+// console.log(process.env.REACT_APP_HOSTED_INDIGO_URL)
+// console.log( process.env.PUBLIC_URL)
 
 let structServiceProvider: any = new RemoteStructServiceProvider(
-    process.env.REACT_APP_HOSTED_INDIGO_URL // see environment in docker-compose
+  process.env.REACT_APP_HOSTED_INDIGO_URL // see environment in docker-compose
 )
-if (process.env.MODE === 'standalone') {
-  const { StandaloneStructServiceProvider } = require('ketcher-standalone')
-  structServiceProvider = new StandaloneStructServiceProvider()
-}
-
-// function sayHello() {
-//   alert('You clicked me!');
-// }
-// // Usage
 
 function App() {
+  // console.log(structServiceProvider)
   return (
-    <div>
-    <p>API_URL: {process.env.REACT_APP_HOSTED_INDIGO_URL}</p>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
     <div>
     <Editor
       staticResourcesUrl={process.env.PUBLIC_URL}
       structServiceProvider={structServiceProvider}
     />
     </div>
-    </div>
+
   );
 }
 
